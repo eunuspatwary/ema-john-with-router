237eeb9e3cfb94064facb69195511e53378f29cc
eunuspatwary <eunuspatwary4@gmail.com>
2021-10-01 20:35:58 +0600

    update

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Inventory
+    = () => {
+        return (
+            <div>
+                <h2>set Inventory</h2>
+            </div>
+        );
+    };
+
+export default Inventory
+    ;
